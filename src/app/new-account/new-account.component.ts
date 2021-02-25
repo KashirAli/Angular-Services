9378b4528f4077a44f94b813cd9be01c92598c63
Kashir ali <bcsf19m518@pucit.edu.pk>
2021-02-25 15:44:44 +0500

    services using dependency injection

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,18 +4,22 @@ import {LoggingService} from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers:[LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private _loggingService:LoggingService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
+    this._loggingService.loggingMsg(accountStatus);
     //console.log('A server status changed, new status: ' + accountStatus);
-    const service = new LoggingService;
-    console.log(service.loggingMsg(accountStatus))
+    //const service = new LoggingService;
+   // console.log(service.loggingMsg(accountStatus))
   }
 }
