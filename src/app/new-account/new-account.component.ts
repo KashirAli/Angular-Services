fef725ead0a598e9930599e4ac85868db185e476
Kashir ali <bcsf19m518@pucit.edu.pk>
2021-02-27 01:21:33 +0500

    creating a data service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,22 +1,20 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
 import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]
+  providers:[LoggingService,AccountService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+ 
 
-  constructor(private _loggingService:LoggingService){}
+  constructor(private _loggingService:LoggingService, private accountService:AccountService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName,accountStatus);
     this._loggingService.loggingMsg(accountStatus);
     //console.log('A server status changed, new status: ' + accountStatus);
     //const service = new LoggingService;
